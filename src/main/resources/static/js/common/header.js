e02fd35f5a0a8766e817d424007c50e5d241d8eb
windwesd <hjkims1122@naver.com>
2024-11-13 10:23:31 +0900

    20241113_[김형진] 헤더 수정

--- src/main/resources/static/js/common/header.js
+++ src/main/resources/static/js/common/header.js
@@ -14,3 +14,28 @@ dep1.addEventListener("mouseleave", () => {
   footer.style.opacity = 1
 })
 
+document.addEventListener("DOMContentLoaded", function () {
+  const galaxyPhoneLink = document.querySelector(".info li a[href='#']");
+  const galaxyContent = document.getElementById("galaxy-content");
+  const dropdown = document.querySelector('.dropdown');
+
+  // 처음에 내용을 숨깁니다.
+  galaxyContent.style.display = 'none';
+
+  // "Galaxy Phone" 클릭 시 토글
+  galaxyPhoneLink.addEventListener("click", function (event) {
+      event.preventDefault(); // 기본 링크 동작을 막음
+
+      // 내용을 토글(보이기/숨기기)합니다.
+      if (galaxyContent.style.display === 'none') {
+          galaxyContent.style.display = 'block';
+      } else {
+          galaxyContent.style.display = 'none';
+      }
+  });
+
+  // 드롭다운 메뉴에서 마우스가 나갔을 때 숨기기
+  dropdown.addEventListener("mouseleave", function () {
+      galaxyContent.style.display = 'none';
+  });
+});
