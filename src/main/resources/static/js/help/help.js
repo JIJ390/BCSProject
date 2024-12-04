7d59fc618b56ff4f94460d4f21c6a4f15c682a78
JIJ390 <wwdi1235@gmail.com>
2024-12-04 10:49:21 +0900

    12321

--- src/main/resources/static/js/help/help.js
+++ src/main/resources/static/js/help/help.js
@@ -11,11 +11,22 @@ pageNoList?.forEach((item, index) => {
 
     const pathname = location.pathname;
 
+    let prevPage = Number(pagination.currentPage) - 1
+    let nextPage = Number(pagination.currentPage) + 1
+
+    if (pagination.currentPage == 1) {
+      prevPage = 1;
+    }
+
+    if (pagination.currentPage == pagination.maxPage) {
+      nextPage = pagination.maxPage;
+    }
+
     // 클릭 버튼 << , < , > , >> 일 때 
     switch(item.innerText){
       case '<<' : location.href = pathname + "?cp=1"; break;
-      case '<'  : location.href = pathname + "?cp=" + pagination.prevPage; break;
-      case '>'  : location.href = pathname + "?cp=" + pagination.nextPage; break;
+      case '<'  : location.href = pathname + "?cp=" + prevPage; break;
+      case '>'  : location.href = pathname + "?cp=" + nextPage; break;
       case '>>' : location.href = pathname + "?cp=" + pagination.maxPage; break;
       default : location.href = pathname + "?cp=" + item.innerText; // 클릭 페이지 이동
     }
@@ -41,4 +52,3 @@ faqDivs.forEach((faqDiv, index) => {
   })
 })
 
-// -------------------------------------------
